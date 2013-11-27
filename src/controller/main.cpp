23c95133e40e649067f7d898234e8b2ed0d2ce3b
Brickgao <brickgao@gmail.com>
2013-11-27 15:34:42 +0800

    finish sendfile

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -3,7 +3,8 @@
 
 #pragma comment(lib, "ws2_32.lib")  
 
-int main() {  
+int main() {
+    printf("Controller start\n");
     WSADATA wsaData;  
     int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);  
     if (iResult != NO_ERROR)  
@@ -24,23 +25,57 @@ int main() {
         closesocket(server);  
         return 0;
     }  
-    if (listen( server, 1 ) == SOCKET_ERROR )  
+    if(listen( server, 1 ) == SOCKET_ERROR)  
         printf("Error listening on socket.\n");  
-    SOCKET AcceptSocket;  
-    printf("Waiting for a client to connect...\n");  
-    while(1) {  
-        AcceptSocket = SOCKET_ERROR;  
-        while ( AcceptSocket == SOCKET_ERROR ) {  
-            AcceptSocket = accept( server, NULL, NULL );  
+    SOCKET AcceptSocket;
+    while(true) {
+        printf("Waiting for a client to connect...\n");  
+        while(1) {  
+            AcceptSocket = SOCKET_ERROR;  
+            while(AcceptSocket == SOCKET_ERROR) {  
+                AcceptSocket = accept(server, NULL, NULL);  
+            }  
+            printf("Client Connected.\n");  
+            server = AcceptSocket;   
+            break;
         }  
-        printf( "Client Connected.\n");  
-        server = AcceptSocket;   
-        break;  
-    }  
-    char sendbuf[1024];
-    DWORD        dwRead;  
-    BOOL         bRet;  
-    send(server, "123",dwRead,0);  
+        char sendbuf[1024];
+        DWORD        dwRead;
+        BOOL         bRet;  
+        char command[100];
+        puts("Print `help` to get help");
+        while(true) {
+            printf(">>");
+            scanf("%s", command);
+            if(command[0] == 's' && command[1] == 'e' && \
+               command[2] == 'n' && command[3] == 'd' && \
+               command[4] == 'f' && command[5] == 'i' && \
+               command[6] == 'l' && command[7] == 'e') {
+                send(server, "sendfile", 8, 0);
+                printf("Ready for sending file :)\ninput filename: ");
+                char filename[100];
+                scanf("%s", filename);
+                HANDLE hFile=CreateFile(filename, GENERIC_READ, 0, 0, \
+                                        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+                while(true) {
+                    bRet = ReadFile(hFile, sendbuf, 1024, &dwRead, NULL);
+                    if(bRet == false) {
+                        puts("Read Buf ERROR!");
+                        break;
+                    }
+                    else if(dwRead == 0) {
+                        puts("Done. Then run it.");
+                        break;
+                    }
+                    else {
+                        send(server, sendbuf, dwRead, 0);
+                    }
+                }
+                CloseHandle(hFile);
+                send(server, "EOF", 3, 0);
+            }
+        }
+    }
     return 0;
 }  
 
